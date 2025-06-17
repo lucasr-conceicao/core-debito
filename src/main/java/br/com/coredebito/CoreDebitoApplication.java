84de8ae6628ea4ccd68aec830e123a068d2eba1b
lucasr-conceicao <lucasr-conceicao@outlook.com.br>
2025-06-16 23:43:04 -0300

    feat: estrutura inicial projeto

---
+++ src/main/java/br/com/coredebito/CoreDebitoApplication.java
@@ -0,0 +1,13 @@
+package br.com.coredebito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreDebitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreDebitoApplication.class, args);
+	}
+
+}
